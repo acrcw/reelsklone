7ecc03eb11cee1d051d912997e7e92c1e674e13f
joban <91016674+acrcw@users.noreply.github.com>
2023-07-10 02:09:13 +0000

    Update 3 files

--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -1,12 +1,11 @@
 import React,{ useContext } from 'react';
 
 import { AuthContext } from '../Context/Authcontext';
-import { BrowserRouter as Router, Route, useNavigate, Routes, Navigate ,Outlet} from 'react-router-dom';
+import { BrowserRouter as  Navigate ,Outlet} from 'react-router-dom';
 function PrivateRoute() 
 {
   const { user } = useContext(AuthContext);
   return user ? <Outlet /> : <Navigate to="/login" />;
-  
 };
 
 export default PrivateRoute;
